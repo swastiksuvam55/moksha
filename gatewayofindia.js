705cb2624412ef9b82a8815dc85864846c8c2c1b
swastiksuvam55 <90003260+swastiksuvam55@users.noreply.github.com>
2022-03-27 03:19:51 +0530

    Add files via upload

--- gatewayofindia.js
+++ gatewayofindia.js
@@ -1,12 +1,12 @@
 function initMap() {
    var options = {
       zoom:15,
-      center: { lat:27.173891, lng: 78.042068} //Coordinates of tajmahal
+      center: { lat:18.922064, lng: 72.834641} //Coordinates of tajmahal
 
    }
 var map = new google.maps.Map(document.getElementById('map'), options);
 var marker = new google.maps.Marker({
-   position:{lat: 27.173891, lng: 78.042068}, // tajmahal Coordinates
+   position:{lat: 18.922064, lng: 72.834641}, // tajmahal Coordinates
    map:map, //Map that is  added
    icon:'https://img.icons8.com/fluent/48/000000/marker-storm.png',
    // adding custom icons
@@ -14,7 +14,7 @@ var marker = new google.maps.Marker({
 });
 
 var information = new google.maps.InfoWindow({
-   content: '<h4>The marker is at Brooklyn</h4>'
+   content: '<h4>The marker is at Gateway of India</h4>'
 });
 
 marker.addListener('click', function() {
